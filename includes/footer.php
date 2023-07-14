39e167e0d9c1c8818aea99e2a7091ded1ccf4a73
Anurag Miglani <125270589+anuragm112@users.noreply.github.com>
2023-07-14 22:41:37 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Lifestyle Store</title>
+  <?php include'links.php';
+  ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            footer{
+                 background-color: #000;
+              color:#fff;
+             font-size:14px;
+              margin-top: 100px;
+            }
+            
+        </style>
+    </head>
+    
+    <body>
+        <footer>  
+        <div class="container">
+            <center>
+        <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</p>
+            </center>
+        </div>
+        </footer>
+    </body>  
+</html>    
